89ee154f668232be5acfc55ba6c4c587e8032f3f
MalikMch <mouchouumalik@gmail.com>
2022-11-02 09:28:45 +0100

    Done

--- server.js
+++ server.js
@@ -1,9 +1,2 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
 require("dotenv").config();
-console.log(`Le serveur tourne sur ${process.env.SERVER_PORT}`);
-console.log(`I am"${process.env.sample.MY_NAME}",wilder in"${process.env.sample.MY_CITY}", and I love"${process.env.sample.MY_LANGUAGE}"`);
-
-serveur.listen(process.env.PORT | 3000);
+console.log(`I am"${process.env.MY_NAME}",wilder in"${process.env.MY_CITY}", and I love"${process.env.MY_LANGUAGE}"`);
